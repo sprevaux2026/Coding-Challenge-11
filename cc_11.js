b2ccf22b9bb98da1447d93460f5ad432b0b076b5
sprevaux2026 <stevenprevaux@gmail.com>
2025-03-02 13:13:01 -0500

    Task 2 - Created Borrower Class

--- cc_11.js
+++ cc_11.js
@@ -21,3 +21,32 @@ const book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", 123456, 5);
 console.log(book1.getDetails());
 book1.updateCopies(-1);
 console.log(book1.getDetails());
+
+// Task 2 - Created Borrower Class
+class Borrower {
+    constructor(name, borrowerId) {
+        this.name = name;
+        this.borrowerId = borrowerId;
+        this.borrowedBooks = [];
+    }
+
+    borrowBook(bookTitle) {
+        this.borrowedBooks.push(bookTitle);
+    }
+
+    returnBook(bookTitle) {
+        const index = this.borrowedBooks.indexOf(bookTitle);
+        if (index !== -1) {
+            this.borrowedBooks.splice(index, 1);
+        } else {
+            console.log(`The book "${bookTitle}" was not borrowed by this user.`);
+        }
+    }
+}
+
+// Test Case 2
+const borrower1 = new Borrower("Alice Johnson", 201);
+borrower1.borrowBook("The Great Gatsby");
+console.log(borrower1.borrowedBooks);
+borrower1.returnBook("The Great Gatsby");
+console.log(borrower1.borrowedBooks);
